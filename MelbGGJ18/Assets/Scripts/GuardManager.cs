308da307a60774572396d6053947cb8f4cd3ea30
AIE\gamejam <lachlan.rm.morrow@gmail.com>
2018-01-27 17:38:28 +1100

    Playin with some stuff

---
+++ MelbGGJ18/Assets/Scripts/GuardManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardManager : MonoBehaviour 
+{
+	public GameObject visionCone;
+	public GameObject[] guardPatrolPoints;
+
+	// Use this for initialization
+	void Start () 
+	{
+		guardPatrolPoints = GameObject.FindGameObjectsWithTag ("GuardPatrolPoint");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
