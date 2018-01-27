e37c71978a15e32de84149ed763ca4ddc28ae45d
AIE\gamejam <lachlan.rm.morrow@gmail.com>
2018-01-28 09:37:06 +1100

    Added script to randomly make one of the Diamonds the target

---
+++ MelbGGJ18/Assets/Scripts/TargetSwapper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetSwapper : MonoBehaviour 
+{
+	public GameObject[] possibleTargets;
+
+//	public GameObject replaceTargetPrefab;
+
+	private int randy;
+	// Use this for initialization
+	void Start () 
+	{
+		possibleTargets = GameObject.FindGameObjectsWithTag ("FalseTarget");
+		randy = Random.Range (0, possibleTargets.Length);
+		possibleTargets [randy].tag = "Target";
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
